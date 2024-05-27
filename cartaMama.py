444fe02c87743bc7524db291761cea85b41e1d5f
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-05-27 12:22:26 -0600

    Create cartaMama.py

---
+++ cartaMama.py
@@ -0,0 +1,29 @@
+def carta_para_mama():
+    nombre_mama = "Mamá"
+    saludo = f"Querida {nombre_mama},"
+    linea = "*" * 50
+    cuerpo_carta = f"""
+    {linea}
+    
+                    Feliz Día de la Madre
+    
+    {linea}
+    
+    Querida Mamá,
+    
+    Espero que este mensaje te encuentre bien. Quiero expresarte lo mucho que significas para mí y lo agradecido que estoy por todo lo que has hecho.
+    
+    Gracias por tu amor incondicional, tu apoyo constante y por ser siempre mi roca en los momentos difíciles. Eres la persona más increíble que conozco, y me siento afortunado de tenerte como madre.
+    
+    En este día especial, quiero desearte todo el amor y la felicidad que te mereces. Espero que este año esté lleno de momentos maravillosos y sueños cumplidos para ti.
+    
+    Te quiero con todo mi corazón, mamá. ¡Feliz día de la madre!
+    
+    Con amor,
+    [Tu nombre]
+    
+    {linea}
+    """
+    print(cuerpo_carta)
+
+carta_para_mama()
